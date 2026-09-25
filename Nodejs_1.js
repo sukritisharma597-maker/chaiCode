b25aae29afcfae89cfe230eb1b032fc2cbd8e505
sukriti sharma <sharmasukriti597@gmail.com>
2026-09-25 18:09:05 +0530

    Initial commit

---
+++ Nodejs_1.js
@@ -0,0 +1,17 @@
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '.env') })
+const express = require('express');
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/instagram', (req, res) => {
+  res.send('SUKRITI HERE GUYSSSS!!')
+})
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+l
